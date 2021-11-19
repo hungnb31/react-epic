6f5fbde137bf757f6f5e6758f04d06d590225059
Hung Nguyen <hungnb31@gmail.com>
2021-11-19 15:33:10 +0700

    react fundamentals: finish exercise 5 - extra credit 1

--- react-fundamentals/src/exercise/05.js
+++ react-fundamentals/src/exercise/05.js
@@ -12,9 +12,37 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const Box = ({className, style, children}) => {
+  return (
+    <div className={`box ${className}`} style={{fontStyle: 'italic', ...style}}>
+      {children}
+    </div>
+  )
+}
+const smallBox = (
+  <Box
+    className="box box--small"
+    style={{fontStyle: 'italic', background: 'lightBlue'}}
+  >
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box
+    className="box box--medium"
+    style={{fontStyle: 'italic', background: 'pink'}}
+  >
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box
+    className="box box--large"
+    style={{fontStyle: 'italic', background: 'orange'}}
+  >
+    large orange box
+  </Box>
+)
 
 function App() {
   return (
