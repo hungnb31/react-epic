f0a5faec4dc963267976e854598815f443030c37
Hung Nguyen <hungnb31@gmail.com>
2022-01-02 10:55:00 +0700

    finished exercise 02 - extra credit: support DOM element

--- advanced-react-patterns/src/exercise/02.js
+++ advanced-react-patterns/src/exercise/02.js
@@ -17,6 +17,23 @@ function Toggle({ children }) {
   return React.Children.map(children, child => {
     // we need to use React.cloneElement here because we need to pass { on, toggle } as props to child components
     // but React doesn't allow to update the props directly, so we need to clone the components and pass props
+
+    // So if we added this, the props on pass to the React Component child
+    // so if we have a DOM element like span below, we not gonna get the error because this not send props to DOM element
+    if (typeof child.type === "string") { // this check is, if child component is DOM element, we just return the child and not pass props
+      return child;
+    }
+
+    // or if we need to only support the set of elemnents, we can have the check like this
+    // const supportedElement  = [ToggleOn, ToggleOff, ToggleButton];
+    // if (supportedElement.includes(child.type)) {
+    //   const cloneChild = React.cloneElement(child, {
+    //     on, toggle
+    //   })
+    //   return cloneChild
+    // }
+    // return child;
+
     const cloneChild = React.cloneElement(child, {
       on, toggle
     })
@@ -35,12 +52,14 @@ const ToggleOff = ({ on, children }) => on ? null : children
 // Accepts `on` and `toggle` props and returns the <Switch /> with those props.
 const ToggleButton = ({ on, toggle }) => <Switch on={on} onClick={toggle} />
 
+// we added a span below to test if our components support DOM element
 function App() {
   return (
     <div>
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Button here</span>
         <ToggleButton />
       </Toggle>
     </div>
