a5cc6d1311f4801ac88750ab29772e1bf0abcbf0
Hung Nguyen <hungnb31@gmail.com>
2022-01-03 22:04:24 +0700

    Prop Collections and Getters - Extra Credit prop getters

--- advanced-react-patterns/src/exercise/04.js
+++ advanced-react-patterns/src/exercise/04.js
@@ -2,27 +2,46 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
-import {Switch} from '../switch'
+import { Switch } from '../switch'
+
+// this function basically synchronously run all provided function if it is truthy
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => fn && fn(...args))
+  }
+}
 
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
+  // instead of directly return props like before
+  // we now create a function to return props
+  // this function allow user to have their own onClick function
+  // but still keep the functionality of the switch by still run toggle function
+  function getTogglerProps({ onClick, ...props } = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props
+    }
+  }
+
   // we return onClick here in order to allow user to toggle the switch
   // we also return "aria-pressed" here to help user doesn't need to care
-  return {on, toggle, togglerProps: {
-    "aria-pressed": on,
-    onClick: toggle
-  }}
+  return {
+    on, getTogglerProps
+  }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const { on, getTogglerProps } = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({ on })} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      {/* user now can define their own onClick function to run extra stuffs */}
+      <button aria-label="custom-button" {...getTogglerProps({ onClick: () => console.info("Function running...") })}>
         {on ? 'on' : 'off'}
       </button>
     </div>
