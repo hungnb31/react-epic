2da6c4059f1b00de6585a8e574df4e51906a4332
Hung Nguyen <hungnb31@gmail.com>
2022-01-12 10:09:44 +0700

    Fix “perf death by a thousand cuts” - Exercise

--- react-performance/src/exercise/06.js
+++ react-performance/src/exercise/06.js
@@ -19,11 +19,6 @@ const initialGrid = Array.from({length: 100}, () =>
 
 function appReducer(state, action) {
   switch (action.type) {
-    // we're no longer managing the dogName state in our reducer
-    // 💣 remove this case
-    case 'TYPED_IN_DOG_INPUT': {
-      return {...state, dogName: action.dogName}
-    }
     case 'UPDATE_GRID_CELL': {
       return {...state, grid: updateGridCellState(state.grid, action)}
     }
@@ -38,8 +33,8 @@ function appReducer(state, action) {
 
 function AppProvider({children}) {
   const [state, dispatch] = React.useReducer(appReducer, {
-    // 💣 remove the dogName state because we're no longer managing that
-    dogName: '',
+    // we have no reason to keep the dog name right here
+    // because it only used in the DogNameInput component
     grid: initialGrid,
   })
   return (
@@ -106,16 +101,9 @@ function Cell({row, column}) {
 Cell = React.memo(Cell)
 
 function DogNameInput() {
-  // 🐨 replace the useAppState and useAppDispatch with a normal useState here
-  // to manage the dogName locally within this component
-  const state = useAppState()
-  const dispatch = useAppDispatch()
-  const {dogName} = state
-
-  function handleChange(event) {
-    const newDogName = event.target.value
-    // 🐨 change this to call your state setter that you get from useState
-    dispatch({type: 'TYPED_IN_DOG_INPUT', dogName: newDogName})
+  const [dogName, setDogName] = React.useState('')
+  const handleChange = (e) => {
+    setDogName(e.target.value)
   }
 
   return (
