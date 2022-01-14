4af291bf9e158e5a961289afa148c9a93ef9b624
Hung Nguyen <hungnb31@gmail.com>
2022-01-14 10:03:04 +0700

    Fix “perf death by a thousand cutsa” - Extra Credit [write an HOC to get a slice of app state]

--- react-performance/src/exercise/06.js
+++ react-performance/src/exercise/06.js
@@ -105,14 +105,17 @@ function Grid() {
 }
 Grid = React.memo(Grid)
 
-function Cell({ row, column }) {
-  const state = useAppState()
-  const cell = state.grid[row][column]
-
-  return <CellImpl cell={cell} row={row} column={column} />
+function withStateSlice(Component, slice) {
+  const MemoComponent = React.memo(Component)
+  function Wrapper(props, ref) {
+    const state = useAppState()
+    return <MemoComponent ref={ref} state={slice(state, props)} {...props} />
+  }
+  Wrapper.displayName = `withStateSlice(${Component.displayName || Component.name}Wrapper)`
+  return React.memo(React.forwardRef(Wrapper))
 }
 
-function CellImpl({ cell, row, column}) {
+function Cell({ state: cell, row, column}) {
   const dispatch = useAppDispatch()
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
   return (
@@ -128,7 +131,7 @@ function CellImpl({ cell, row, column}) {
     </button>
   )
 }
-CellImpl = React.memo(CellImpl)
+Cell = withStateSlice(Cell, (state, { row, column }) => state.grid[row][column])
 
 function DogNameInput() {
   const [state, dispatch] = useDog()
