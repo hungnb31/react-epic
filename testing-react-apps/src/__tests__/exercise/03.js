e0b02ab853c147b715ff58d448761d2399e25a6e
Hung Nguyen Ba <hungnb31@gmail.com>
2022-02-14 17:58:38 +0700

    feature: finished exercise 3

--- testing-react-apps/src/__tests__/exercise/03.js
+++ testing-react-apps/src/__tests__/exercise/03.js
@@ -3,14 +3,25 @@
 
 import * as React from 'react'
 // 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+import {render, fireEvent, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
   const {container} = render(<Counter />)
   // 🐨 replace these with screen queries
   // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
+  // if we add another button to the component
+  // so the first button would not to be the decrement button
+  // and the second also not increment button
+  // const [decrement, increment] = container.querySelectorAll('button')
+
+  // we have this way to directly query to the increment and decrement button
+  const increment = screen.getByText("Increment");
+  const decrement = screen.getByText("Decrement");
+
+  // OR
+  // const increment = screen.getByRole('button', { name: /increment/i})
+  // const decrement = screen.getByRole('button', { name: /decrement/i})
   const message = container.firstChild.querySelector('div')
 
   expect(message).toHaveTextContent('Current count: 0')
