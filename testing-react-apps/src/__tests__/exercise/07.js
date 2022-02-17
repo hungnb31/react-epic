55a48fc9ebac7d3e4e2dfd6891e15cdcdee5a8ea
Hung Nguyen Ba <hungnb31@gmail.com>
2022-02-17 23:28:45 +0700

    feature: finished exercise 7

--- testing-react-apps/src/__tests__/exercise/07.js
+++ testing-react-apps/src/__tests__/exercise/07.js
@@ -7,13 +7,18 @@ import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
+  const Wrapper = ({ children }) => {
+    return <ThemeProvider initialTheme="light">{children}</ThemeProvider>
+  }
+  // using wrapper here instead of directly wrap button with <ThemeProvider>
+  // because it easier when we want to test other provider
+  // so it easier to switch between providers
+  render(<EasyButton>Easy</EasyButton>, { wrapper: Wrapper })
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
   //
   // 🐨 update the `render` call above to use the wrapper option using the
   // ThemeProvider
