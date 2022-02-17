21034f55a4618e3c35ef55112172286c7e07a05f
Hung Nguyen Ba <hungnb31@gmail.com>
2022-02-17 23:30:56 +0700

    feature: finished exercise 7 extra credit 1

--- testing-react-apps/src/__tests__/exercise/07.js
+++ testing-react-apps/src/__tests__/exercise/07.js
@@ -19,9 +19,19 @@ test('renders with the light styles for the light theme', () => {
     background-color: white;
     color: black;
   `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+})
+
+// test the dark theme
+test('renders with the dark styles for the dark theme', () => {
+  const Wrapper = ({ children }) => {
+    return <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  }
+  render(<EasyButton>Easy</EasyButton>, { wrapper: Wrapper })
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    color: white;
+    background-color: black;
+  `)
 })
 
 /* eslint no-unused-vars:0 */
